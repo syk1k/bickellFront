c1f6d1ab9623f7d86b86d2b2277cbab1e2fc3197
thanku <thanku@localhost.localdomain>
2018-07-16 02:28:22 -1000

    Set title to demandes and offres pages through their constructor

--- src/app/demandes/demandes.component.ts
+++ src/app/demandes/demandes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-demandes',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DemandesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private title: Title) { 
+    this.title.setTitle("Demandes");
+  }
 
   ngOnInit() {
   }
